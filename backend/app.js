e7891ce3679610152d806dc779e8e28461fcc451
neeraj10122004 akula <neerajsaiteja@gmail.com>
2024-12-15 13:43:56 +0000

    back

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,15 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+  const { matrix } = req.body;
+  console.log('Received Matrix:', matrix);
+  res.json({ message: 'Matrix received successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
--- frontend/src/components/Gridstyle.jsx
+++ frontend/src/components/Gridstyle.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import { Sympmatrix } from './Sympmatrix'
 import { useState } from 'react'
 import { useRef } from 'react'
+import axios from 'axios'
 
 export const Gridstyle = () => {
     const [output, setoutput] = useState(false)
     const [matrix, setMatrix] = useState(
         Array.from({ length: 11 }, () => Array(12).fill(0))
       );
-    const submit = () =>{
-        console.log(matrix);
-        setoutput(true);
-        setMatrix(Array.from({ length: 11 }, () => Array(12).fill(0)));
+    const submit = async () =>{
+        try {
+            const response = await axios.post('https://5000-neeraj10122004-medihelp-44vvu9arfl4.ws-us117.gitpod.io/submit', {
+              matrix // Send the matrix as part of the request body
+        });
+            console.log('Response from server:', response.data);
+            console.log(matrix);
+            setoutput(true);
+            setMatrix(Array.from({ length: 11 }, () => Array(12).fill(0)));
+        } catch (error) {
+            console.error('Error submitting the matrix:', error);
+        }
+        
         
     }
     
